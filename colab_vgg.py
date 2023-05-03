28c2ea9b89aa412fbb623cbfb86fb369c8993cba
BrianPark314 <everfrost314@gmail.com>
2023-05-03 09:27:48 +0900

    added transformations for vgg16

--- colab_vgg.py
+++ colab_vgg.py
@@ -21,9 +21,9 @@ args.NUM_EPOCHS = 100
 args.path = Path("/content/gdrive/MyDrive/project/Dacon_tile/data/")
 args.transform = transforms.Compose([ 
         transforms.Resize((224, 224)),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
 class ClassifierModule(nn.Module):
