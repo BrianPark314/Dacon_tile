5adb73e7f227b7230efd0eaad10e259aa85a213b
BrianPark314 <everfrost314@gmail.com>
2023-05-03 09:22:43 +0900

    added transformations for vgg16

--- colab_vgg.py
+++ colab_vgg.py
@@ -16,11 +16,13 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 
 print(f'Current device is: {device}')
 args = easydict.EasyDict()
-args.BATCH_SIZE = 128
+args.BATCH_SIZE = 64
 args.NUM_EPOCHS = 100
 args.path = Path("/content/gdrive/MyDrive/project/Dacon_tile/data/")
 args.transform = transforms.Compose([ 
-        transforms.Resize((64, 64)),
+        transforms.Resize((224, 224)),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.RandomHorizontalFlip(p=0.5),
         transforms.ToTensor(),
     ])
 
