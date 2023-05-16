7156c5b7d3bb843ead4b2aac46116b23f33921f1
BrianPark314 <everfrost314@gmail.com>
2023-05-16 15:29:20 +0900

    SAM optimizer added, CustomImageFolder depricated, fixed test.py

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ print(f'Current device is: {device}')
 
 def go(model, train_data, validation_data, label):
     loss_fn = nn.CrossEntropyLoss()
-    base_optimizer = torch.optim.Adam
+    base_optimizer = torch.optim.SGD
     optimizer = eng.SAM(model.parameters(), base_optimizer, lr=args.lr, momentum=0.9)
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
                                                 step_size = 5,
