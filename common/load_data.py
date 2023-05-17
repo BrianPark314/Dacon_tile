738b2f955613e7a21e74222a49743258d8006ca2
BrianPark314 <everfrost314@gmail.com>
2023-05-17 15:20:36 +0900

    reset data preprocessing

--- common/load_data.py
+++ common/load_data.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from common.params import args
 
 def get_train_dataloader(BATCH_SIZE, path, transform: transforms):
-    train_dir = path / "train/"
+    train_dir = path / "_processed_train/"
     train_data = datasets.ImageFolder(train_dir, transform)
     targets = train_data.targets
     label = train_data.class_to_idx
@@ -26,9 +26,9 @@ def get_train_dataloader(BATCH_SIZE, path, transform: transforms):
     return train_dataloader, valid_dataloader, label
 
 def get_test_dataloader(BATCH_SIZE, path, transform_test: transforms):
-    train_dir = path / "train/"
+    train_dir = path / "_processed_train/"
     label = datasets.ImageFolder(train_dir).class_to_idx
-    test_dir = path / "test/"
+    test_dir = path / "_processed_test/"
     test_data = datasets.ImageFolder(test_dir, transform_test)
     test_dataloader = DataLoader(test_data, batch_size = BATCH_SIZE, num_workers=0)
     
